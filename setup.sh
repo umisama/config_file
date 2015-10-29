e9ef2f2116aa02e3a29c04d16dc8b37a3771ab79
umisama <umisama@fe2o3.jp>
2015-10-29 17:13:35 +0900

    Fix install neobundle failed

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
 
-sudo apt-get install -y byobu zsh gvim
+sudo apt-get install -y byobu zsh vim-gnome make gcc
 
 # file copy(symbolic)
 FILE_NAMES=( ".vimrc" ".vimrc.command" ".vimrc.office" ".zshrc" ".gitconfig" )
 for file in ${FILE_NAMES[@]}
 	ln -s $HOME/config_file/$file $HOME/$file
 
-# install neobundle
-git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle
+# install NeoBundle for vim
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh install.sh
+rm install.sh
 
 # change default shell (->zsh)
 chsh -s /bin/zsh
