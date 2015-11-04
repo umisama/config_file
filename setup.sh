b9d1e90f917f4c9e1c8cce2532a24f192f49df2e
umisama <umisama@fe2o3.jp>
2015-11-04 14:35:57 +0900

    oh my zsh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y byobu zsh vim-gnome make gcc
 
 # file copy(symbolic)
-FILE_NAMES=( ".vimrc" ".vimrc.command" ".vimrc.office" ".zshrc" ".gitconfig" )
+FILE_NAMES=( ".vimrc" ".vimrc.command" ".vimrc.office" ".zshrc_myconfigs" ".gitconfig" )
 for file in ${FILE_NAMES[@]}
 	ln -s $HOME/config_file/$file $HOME/$file
 
@@ -11,6 +11,10 @@ for file in ${FILE_NAMES[@]}
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh install.sh
 rm install.sh
+
+# oh my zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo "source .zshrc_myconfigs" >> ~/.zshrc
 
 # change default shell (->zsh)
 chsh -s /bin/zsh
