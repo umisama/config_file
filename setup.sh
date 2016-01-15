b2f272db1bf81c7ba9e28b4144fe87adc2d7a3f0
umisama <umisama@fe2o3.jp>
2016-01-15 14:36:57 +0900

    [setup]Add nvm and rvm

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-
 sudo apt-get install -y byobu zsh vim-gnome make gcc
 
 # file copy(symbolic)
@@ -15,6 +14,14 @@ rm install.sh
 # oh my zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "source .zshrc_myconfigs" >> ~/.zshrc
+
+# nodejs
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+
+# Ruby version manager
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+
 
 # change default shell (->zsh)
 chsh -s /bin/zsh
